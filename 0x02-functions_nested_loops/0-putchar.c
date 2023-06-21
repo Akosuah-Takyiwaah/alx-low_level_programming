27d8d1a9509ed3be0b81557d4e0aa29fc5b33e52
Akosuah-Takyiwaah <danquahakosuah@gmail.com>
2023-06-21 15:09:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main .h"
+
+/**
+ * main - Entry point
+ * Return: Always
+ */
+
+int main(void)
+
+{
+
+         _putchar(' ');
+         _putchar('p');
+         _putchar('u');
+         _putchar('t');
+         _putchar('c');	 
+	 _putchar('h');
+	 _putchar('o');
+	_putchar('\n')
+		return (n);
+}
