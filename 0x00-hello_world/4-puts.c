298fb6022fc4f6bad37c987a3e23c0be747dba1c
Akosuah-Takyiwaah <danquahakosuah@gmail.com>
2023-07-11 00:53:10 -0700

    tasks done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line with puts function
- * Return :0(Success)
+ * main - Entry point
+ *
+ * Return: Ending with a value of 0
  */
 int main(void)
-{	
-puts ("\"Programming is like building a multilingual puzzle");
-return (0);
+{
+	char Multi[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(Multi);
+
+	return (0);
 }	
